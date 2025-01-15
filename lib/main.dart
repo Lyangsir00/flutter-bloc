e892f14032e5765e5de19d5d7072931537a73974
Susant gurung <susantgurung@Suman-MacBook-Air.local>
2025-01-15 14:03:16 +0545

    feat: add bloc to bloc communication with streamsubscription

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_example2/bloc/counterapp/counterapp_bloc.dart';
 import 'package:flutter_bloc_example2/bloc/color/color_bloc.dart';
 import 'package:flutter_bloc_example2/ui/bloc_to_bloc_home.dart';
+import 'package:flutter_bloc_example2/ui/stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: BlocToBlocHome(),
+        home: StreamPage(),
       ),
     );
   }
--- lib/ui/stream.dart
+++ lib/ui/stream.dart
@@ -1,10 +1,98 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Stream extends StatelessWidget {
-  const Stream({super.key});
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_example2/bloc/color/color_bloc.dart';
+import 'package:flutter_bloc_example2/bloc/counterapp/counterapp_bloc.dart';
+
+class StreamPage extends StatefulWidget {
+  const StreamPage({super.key});
+
+  @override
+  State<StreamPage> createState() => _StreamState();
+}
+
+class _StreamState extends State<StreamPage> {
+  StreamSubscription<ColorState>? _streamSubscription;
+
+  void initState() {
+    super.initState();
+    _streamSubscription = context.read<ColorBloc>().stream.listen(
+      (state) {
+        context.read<CounterappBloc>().add(
+              UpdateCounter(color: state.color),
+            );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bloc using StreamSubscription"),
+      ),
+      body: BlocBuilder<ColorBloc, ColorState>(
+        builder: (context, state) {
+          return Container(
+            height: 200,
+            width: double.infinity,
+            color: state.color,
+            child: Center(child: BlocBuilder<CounterappBloc, CounterappState>(
+              builder: (context, state) {
+                return Text(
+                  state.counter.toString(),
+                  style: TextStyle(fontSize: 30, color: Colors.pink),
+                );
+              },
+            )),
+          );
+        },
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              context.read<ColorBloc>().add(ChangeColor(color: Colors.green));
+            },
+            child: Text(
+              "Green",
+              style: TextStyle(color: Colors.green),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.read<ColorBloc>().add(ChangeColor(color: Colors.blue));
+            },
+            child: Text("Blue", style: TextStyle(color: Colors.blue)),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.read<ColorBloc>().add(ChangeColor(color: Colors.red));
+            },
+            child: Text(
+              "Red",
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.read<ColorBloc>().add(ChangeColor(color: Colors.black));
+            },
+            child: Text(
+              "Black",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
