1cdc443b12320944e9a58ce392a4aaf8ec4d4574
Susant gurung <susantgurung@Suman-MacBook-Air.local>
2025-01-16 13:04:35 +0545

    feat: example to implement bloc consumer

---
+++ lib/ui/bloc_consumer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BlocConsumer extends StatefulWidget {
+  const BlocConsumer({super.key});
+
+  @override
+  State<BlocConsumer> createState() => _BlocConsumerState();
+}
+
+class _BlocConsumerState extends State<BlocConsumer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
